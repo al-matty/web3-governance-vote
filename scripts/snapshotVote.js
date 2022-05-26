d08477353f907169d5c437a23685038cca15b2c1
Al Matty <al@almatty.com>
2022-05-25 19:16:26 -0500

    Some first debugging done

--- scripts/snapshotVote.js
+++ scripts/snapshotVote.js
@@ -1,26 +1,23 @@
-// workaround to allow require statements while {type: module}
+// workaround to allow require statements with node.js while {type: module}
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-// workaround to allow __dirname while {type: module}
+// workaround to allow __dirname with node.js while {type: module}
 const path = require("path");
 const __dirname = path.resolve(path.dirname(''));
 
-// import/require statements
+// imports
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 const Web3 = require('web3');
 const fs = require("fs");
 const { ethers } = require("ethers");
 import snapshot from '@snapshot-labs/snapshot.js';
-const hub = 'https://hub.snapshot.org'; // or https://testnet.snapshot.org for testnet
-const client = new snapshot.Client712(hub);
 
-// define paths
+// paths
 const choicesPath = path.join(__dirname, '..', '..', 'snapshot-query', 'choices.json');
 const encrPkPath = path.join(__dirname, '..', '..', 'test_wallet_encrPK.json');
 const INFURA_KEY = process.env.WEB3_INFURA_PROJECT_ID;
 const ENCR_PW = process.env.ENCR_PW;
-const choices = require(choicesPath);
 
 // decrypt ETH wallet
 const rpcURL = "https://mainnet.infura.io/" + INFURA_KEY;
@@ -33,7 +30,8 @@ function getAccount (pathToEncrPk, encrPw) {
 const account = getAccount(encrPkPath, ENCR_PW);
 
 // instantiate signer with wallet keys
-const { ethers } = require("ethers");
+const hub = 'https://hub.snapshot.org'; // or https://testnet.snapshot.org for testnet
+const client = new snapshot.Client712(hub);
 const INFURA_SECRET = process.env.WEB3_INFURA_PROJECT_SECRET;
 const provider = new ethers.providers.InfuraProvider("homestead", INFURA_KEY)
 const wallet = new ethers.Wallet(account.privateKey, provider)
@@ -41,17 +39,18 @@ var signer = wallet.connect(provider)
 
 // convert voting data to right format
 const getVoteDict = (proposal) => {
-  outObj = {
+  return {
     space: proposal['space'],
     proposal: proposal['id'],
     type: proposal['type'],
     choice: proposal['pop_choice'],
     metadata: JSON.stringify({})
   };
-  return outObj;
 };
 
 // vote with each wallet as determined in choices.json
+const choices = require(choicesPath);
+
 for (let wallet_proposals of Object.entries(choices)) {
   wallet = wallet_proposals[0];
   proposals = wallet_proposals[1];
